53790b370df7776a5e07405085d950f683fb2448
Serafym Syniepolskyi <projektive28.11@gmail.com>
2022-06-07 10:40:05 +0300

    wai-aria

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,31 @@
-(function() {
+(function () {
+  setInterval(function () {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes()
+    let time = String(`${hours}:${minutes}`)
+    console.log(time);
+    document.getElementById("ariaLiveExample").textContent = time;
+  }, 30000);//there is an opportunity to put 60000ms, but then the idea with the digital clock will be extremely strange :)
+})();
+
+document.querySelectorAll("#nav li").forEach(function (navEl) {
+  navEl.onclick = function () {
+    toggleTab(this.id, this.dataset.target);
+  };
+});
+
+(function () {
   var burger = document.querySelector(".burger");
   var menu = document.querySelector("#" + burger.dataset.target);
-  burger.addEventListener("click", function() {
+  burger.addEventListener("click", function () {
     burger.classList.toggle("is-active");
     menu.classList.toggle("is-active");
   });
 })();
 
-document.querySelectorAll("#nav li").forEach(function(navEl) {
-  navEl.onclick = function() {
+document.querySelectorAll("#nav li").forEach(function (navEl) {
+  navEl.onclick = function () {
     toggleTab(this.id, this.dataset.target);
   };
 });
@@ -16,7 +33,7 @@ document.querySelectorAll("#nav li").forEach(function(navEl) {
 function toggleTab(selectedNav, targetId) {
   var navEls = document.querySelectorAll("#nav li");
 
-  navEls.forEach(function(navEl) {
+  navEls.forEach(function (navEl) {
     if (navEl.id == selectedNav) {
       navEl.classList.add("is-active");
     } else {
@@ -28,7 +45,7 @@ function toggleTab(selectedNav, targetId) {
 
   var tabs = document.querySelectorAll(".tab-pane");
 
-  tabs.forEach(function(tab) {
+  tabs.forEach(function (tab) {
     if (tab.id == targetId) {
       tab.style.display = "block";
     } else {
